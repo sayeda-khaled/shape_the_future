9c0dff74cdd60b6710b50c035f9d928926270dd8
Sayeda-Khaled <ksayeda1@gmail.com>
2021-07-11 12:52:36 -0400

    fixed the migrations and updated the events model

--- events/migrations/0007_auto_20210711_1317.py
+++ events/migrations/0007_auto_20210711_1317.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='event',
             name='duration',
-            field=models.DurationField(default='01:00:00'),
+            field=models.DurationField(),
         ),
         migrations.AddField(
             model_name='event',
