77cce0c449dda9c6aaec59ec515b8047c612e976
Sayeda-Khaled <ksayeda1@gmail.com>
2021-07-11 15:40:06 -0400

    the volunteer signed up events have the from and to date

--- frontend/static/src/components/VolunteerPage.js
+++ frontend/static/src/components/VolunteerPage.js
@@ -94,12 +94,19 @@ class VolunteerPage extends Component {
           <section >
               <form onSubmit={(e) => this.signUp(e, event)} className="form-1 bg-purple-100 m-6 p-4 rounded-3xl m-auto block max-w-md mt-6">
                 <div class="mb-3">
-                  <label for="exampleFormControlInput1" class="form-label text-gray-500 block text-sm mr-2">Grade:</label>
-                  <p class="form-control pb-3" id="exampleFormControlInput1" name="grade">{event.grade} </p>
+                    <label for="exampleFormControlInput1" class="form-label text-gray-500 block text-sm mr-2">Grade:</label>
+                    <p class="form-control pb-3" id="exampleFormControlInput1" name="grade">{event.grade} </p>
 
 
-                  <label for="exampleFormControlTextarea1" class="form-label text-gray-500 block text-sm mr-2">Event Date:</label>
-                  <time class="form-control pb-3" id="exampleFormControlInput1" rows="3">{event.date_of_event}</time>
+                    <label for="exampleFormControlTextarea1" class="form-label text-gray-500 block text-sm mr-2">Event Date:</label>
+                    <time class="form-control pb-3" id="exampleFormControlInput1" rows="3">{event.date_of_event}</time>
+
+
+                    <label for="exampleFormControlTextarea1" class="form-label text-gray-500 block text-sm mr-2">From:</label>
+                    <time class="form-control pb-3" id="exampleFormControlInput1" rows="3">{event.start_of_event}</time>
+
+                    <label for="exampleFormControlTextarea1" class="form-label text-gray-500 block text-sm mr-2">To::</label>
+                    <time class="form-control pb-3" id="exampleFormControlInput1" rows="3">{event.end_of_event}</time>
 
                 <div class="btn-signup bg-blue pb-2"type ='button' onClick={(e) => this.cancelEvent(e,event)}>Cancel</div>
             </div>
