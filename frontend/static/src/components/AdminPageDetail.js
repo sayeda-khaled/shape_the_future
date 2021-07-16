bc304e2365ad67533486ccb559a859b493a519c0
Sayeda-Khaled <ksayeda1@gmail.com>
2021-07-16 13:52:57 -0400

    refactoring my admin page

--- frontend/static/src/components/AdminPageDetail.js
+++ frontend/static/src/components/AdminPageDetail.js
@@ -7,7 +7,7 @@ class AdminPageDetail extends Component {
 
     this.state = {
       ...this.props.event,
-      isEditing: false,
+      isEditing: false
     }
 
     this.handleInput = this.handleInput.bind(this);
@@ -41,17 +41,28 @@ class AdminPageDetail extends Component {
     const students = this.props.student;
     const options = this.props.students.map(student => <option value={student.id} selected={event.student === student.id}>{`${student.last_name}, ${student.first_name}`}</option>)
     return (<li className="bg-purple-100 m-6 p-4 rounded w-80">
+      <div>
+        {
+          this.state.isEditing
+            ? <button class="btn-edit bg-blue flex-col ml-2 mt-2" type='button' onClick={this.saveEvent}>Save</button>
+            : <button class="btn-edit bg-blue flex-col ml-2 mt-2 rounded-full py-3 px-6" onClick={() => this.setState({isEditing: true})}>Edit Event</button>
+        }
+        <button class="btn-edit bg-blue rounded-full py-3 px-6" onClick={(e) => this.props.deleteEvent(e, event.id)}>delete Event</button>
+      </div>
+
       <div className="flex items-center">
         <label className="text-gray-500 block text-sm mr-2">Grade:</label>
         <input type="text" name="grade" value={this.state.grade} onChange={this.handleInput} disabled={!this.state.isEditing}/>
       </div>
       <label className="text-gray-500 block text-sm">Date:</label>
-      <time>{event.date_of_event}</time>
+      <input type="date" min="2021-7-07" max="2021-12-31" class="input-1" name="date" value={this.state.date_of_event} onChange={this.handleInput} disabled={!this.state.isEditing}/>
+
       <div className="flex items-center">
         <label className="text-gray-500 block text-sm">From:</label>
-        <time className="mr-4">{event.start_of_event}</time>
+        <input type="time" className="input-1" name="startTime" value={this.state.start_of_event} onChange={this.handleInput} disabled={!this.state.isEditing}/>
         <label className="text-gray-500 block text-sm ">To:</label>
-        <time>{event.end_of_event}</time>
+        <input type="time" className="input-1" name="endTime" value={this.state.end_of_event} onChange={this.handleInput} disabled={!this.state.isEditing}/>
+
       </div>
       <label className="text-gray-500 block text-sm">Volunteer:</label>
       <p>{event.volunteer_name}</p>
@@ -62,19 +73,10 @@ class AdminPageDetail extends Component {
         </option>
         {options}
       </select>
-      <button type="button" onClick={this.assignStudent}>Assign student</button>
+      <button type="button" className="btn-submit" onClick={this.assignStudent}>Assign student</button>
 
-      <button class="btn-edit bg-blue rounded-full py-3 px-6" onClick={(e) => this.props.deleteEvent(e, event.id)}>delete</button>
-      {
-        this.state.isEditing
-           ? <button class="btn-edit bg-blue flex-col ml-2 mt-2" type='button' onClick={this.saveEvent}>Save</button>
-           : <button class="btn-edit bg-blue flex-col ml-2 mt-2 rounded-full py-3 px-6" onClick={() => this.setState({isEditing: true})}>Edit</button>
-      }
-
-    </li>
-    )
+    </li>)
   }
 }
-
 
 export default AdminPageDetail;
